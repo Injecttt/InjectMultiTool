f1b80e7c95a01157028f023f33213a7783dd6cc4
Injecttt <127987941+Injecttt@users.noreply.github.com>
2023-03-15 19:54:02 +0100

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,21 @@
+
+       
+                                       ~ Inject.qlf#6666             
+                                       ~ @inject.qlf
+                                                                                                                                                    
+
+           _____________________________________________________________________________________________
+          | [1] Pinger                       [10] Rat/Virus Creator(Github)  [19] Rat Tutorial          |
+          | [2] Ip Stresser                  [11] Keyword Generator Tool     [20] Sick Fonts (Text etc.)|
+          | [3] Ip Lookup                    [12] Vpn/Ovh                    [21] Phone Spoofer (SSL)   | 
+          | [4] Port Scanner                 [13] Open Cyberhub(Hacking)     [22] Sms Bomber (iOS)      | 
+          | [5] Open Putty                   [14] Open Vedbex(USEFUL TOOLS)  [23] Ip-Logger             |
+          | [6] Dox Tool                     [15] Ascii Art                  [24] Botting Panel         |
+          | [7] USA Fake Info Generator      [16] Random Ip Generator        [25] Instagram ID Searcher |
+          | [8] 30+ Phishing Sites           [17] Steam Account Generator    [26] Fortnite Cheat        |
+          | [9] Extreme Doxing Tool          [18] Minecraft-Spotify Generator[27]  EXIT                 |
+          |_____________________________________________________________________________________________|   
+  
+  
+   
+    
